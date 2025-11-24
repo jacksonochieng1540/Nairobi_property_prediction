b8134234dc4ad3a302c377d1ef2f4a185ebefee2
Jack ayaga <jacksonochieng1540@gmail.com>
2025-11-24 21:47:05 +0300

    Update urls.py

--- nairobi_property_predictor/urls.py
+++ nairobi_property_predictor/urls.py
@@ -23,7 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('property_predictor.urls')),
 ]
-
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
